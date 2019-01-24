7f33461e3029ff96f84999f0839012484cc53b2d
theavee <aveechakra@gmail.com>
2019-01-24 19:58:45 +0600

    thesis work updated

---
+++ thesis-work/Userdriven_composition/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Actuator,LowerSensor
+# Register your models here.
+admin.site.register(Actuator)
+admin.site.register(LowerSensor)
+
---
+++ thesis-work/static/js/generators/codegenerator-weather.js
@@ -0,0 +1,6 @@
+
+Blockly.Python['weather'] = function(Block) {
+    var code = "parseJson()";
+    return [code, Blockly.Python.ORDER_ATOMIC];
+    //return code;
+  }
